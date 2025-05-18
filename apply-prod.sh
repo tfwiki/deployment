d6e30abde7ad4bb4611c745679b0fc5fedefcdb5
Etienne Perot <etienne@perot.me>
2025-05-18 16:04:38 -0700

    Bring kubernetes objects back in line with their actual state.

--- apply-prod.sh
+++ apply-prod.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-kubectl delete job mediawiki-update
-kubectl apply -f k8s/common/mediawiki-update.yml
+#kubectl delete job mediawiki-update
+#kubectl apply -f k8s/common/mediawiki-update.yml
 kubectl apply -f k8s/common/mediawiki.yaml
 kubectl apply -f k8s/common/run-jobs.yaml
 kubectl apply -f k8s/common/update-special-pages.yaml
